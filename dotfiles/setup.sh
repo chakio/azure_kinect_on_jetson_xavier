39f26f31cf24b66335ac029f8ea8afce16135131
chakio <y-kawasaki@keio.jp>
2020-03-24 15:27:37 +0900

    FIX scripts

--- dotfiles/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./docker_setting.sh
-./fun_setting.sh
-./japanese_setting.sh
-./vscode_for_arm_setting.sh
--- scripts/fun_setting.sh
+++ scripts/fun_setting.sh
@@ -2,5 +2,5 @@
 
 echo "# fun setting"
 sudo  nvpmodel  -m  0 && sudo jetson_clocks
-sudo echo "sudo  nvpmodel  -m  0 && sudo jetson_clocks" > /etc/rc.local
+sudo echo -e "#!/bin/sh -e \n nvpmodel  -m  0 && jetson_clocks \n exit 0" >> /etc/rc.local
 sudo chmod u+x /etc/rc.local
