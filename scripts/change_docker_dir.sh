08e20aee9bc3b6d16ff0edf5bb163ff7d9ca17ac
chakio <y-kawasaki@keio.jp>
2020-03-28 23:10:29 +0900

    ADD: change docker directory

--- scripts/change_docker_dir.sh
+++ scripts/change_docker_dir.sh
@@ -1,8 +1,8 @@
 echo "Stop Docker"
 sudo /etc/init.d/docker stop
 echo "Move Docker Images to Destination"
-sudo mv /var/lib/docker /media/ytjs2020d/ssd
+sudo mv /var/lib/docker /media/ytjs2020d/ssd/
 echo "Make Symbolic Link to Destination"
-sudo ln -s /media/ytjs2020d/ssd /var/lib/docker/
+sudo ln -s /media/ytjs2020d/ssd/docker/ /var/lib/docker
 echo "Restart Docker"
 sudo /etc/init.d/docker start
